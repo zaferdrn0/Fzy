8dc5dca83c2ddb937537b2fe810fa6337f6b97e2
zaferdrn0 <zaferdrn0@gmail.com>
2024-11-16 00:27:46 +0300

    docker file created and updated for mongodb connections

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -1,14 +1,20 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_ADDRESS || 'mongodb://localhost:27017/yourDatabaseName', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    throw error; 
+dotenv.config();
+
+export const connectDB = async (retries = 5, delay = 5000) => {
+  while (retries) {
+    try {
+      await mongoose.connect(process.env.MONGODB_ADDRESS);
+      console.log('MongoDB Connected');
+      return;
+    } catch (error) {
+      console.error('MongoDB connection error:', error);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      if (!retries) throw new Error('Could not connect to MongoDB');
+      await new Promise((res) => setTimeout(res, delay));
+    }
   }
 };
--- backend/src/config/session.js
+++ backend/src/config/session.js
@@ -2,11 +2,13 @@ import MongoStore from 'connect-mongo';
 import session from 'express-session';
 
 export const sessionMiddleware = session({
-    secret: process.env.SESSION_KEY,
-    resave: false, 
-    saveUninitialized: false, 
-    store: MongoStore.create({ mongoUrl: process.env.MONGODB_ADDRESS }),
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 
-    }
-  })
+  secret: process.env.SESSION_KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_ADDRESS
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
+});
