e5f8e9e5980ad378ff3cdaacb376093f8b4ba4e8
zaferdrn0 <zaferdrn0@gmail.com>
2024-11-18 22:14:21 +0300

    Layout system was created and layout was removed from the login.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,27 +1,41 @@
 import Sidebar from "@/components/Sidebar";
 import { AuthProvider } from "@/context/AuthContext";
 import { menuItems } from "@/navbar/navbarMenu";
-//import Header from "@/layout/Header";
 import "@/styles/globals.css";
 import theme from "@/theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
-export default function App({ Component, pageProps }: AppProps) {
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => (
+    <Box sx={{ display: "flex", height: "100vh", overflow: "hidden" }}>
+      {/* Sidebar */}
+      <Box className="sidebar" sx={{ flexShrink: 0 }}>
+        <Sidebar menuItems={menuItems} />
+      </Box>
+      {/* Content */}
+      <Box sx={{ flexGrow: 1, overflowY: "auto", p: 2 }}>
+        {page}
+      </Box>
+    </Box>
+  ));
 
   return (
     <AuthProvider>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-    <Box sx={{ display: 'flex' }}>
-    <Sidebar menuItems={menuItems} />
-        <Box sx={{ flexGrow: 1 ,m:2}}>
-          <Component {...pageProps} />
-        </Box>
-      </Box>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
     </AuthProvider>
-  )
-
+  );
 }
